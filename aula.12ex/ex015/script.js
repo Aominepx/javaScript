9018bb122e662e8b56312d8a2a9d99a4baf8e38d
David Felipe <davidfelipe385@gmail.com>
2024-08-08 16:08:48 -0300

    atualizado

--- aula.12ex/ex015/script.js
+++ aula.12ex/ex015/script.js
@@ -6,16 +6,37 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano){
         window.alert('Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsByClassName('radsex') 
+        var fsex = document.getElementsByName('radsex') 
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             gênero = 'Homen'
+            if (idade >=0 && idade <10){
+                img.setAttribute('src', 'img/baby-men.jpeg')
+            } else if (idade < 21){
+                img.setAttribute('src', 'img/young-men.jpeg')
+            } else if ( idade < 50){
+                img.setAttribute('src', 'img/men.jpeg')
+            } else {
+                img.setAttribute('src', 'img/old-men.jpeg')
+            }
         } else if (fsex[1].checked){
             gênero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                img.setAttribute('src', 'img/baby-girl.jpeg')
+            } else if (idade < 21){
+                img.setAttribute('src', 'img/young-woman.jpeg')
+            } else if ( idade < 50){
+                img.setAttribute('src', 'img/woman.jpeg')
+            } else {
+                img.setAttribute('src', 'img/old-woman.jpeg')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 
 }
